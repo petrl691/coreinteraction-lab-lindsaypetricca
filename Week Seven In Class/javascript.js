a59bf2b97603c96da48b14389f6d8d56a0550e79
Lindsay Petricca <petrl691@newschool.edu>
2016-03-16 15:47:25 -0400

    commit

---
+++ Week Seven In Class/javascript.js
@@ -0,0 +1,23 @@
+var hello = hello world;
+
+console.log(hello);
+
+
+var a = 'lindsay';
+var b = 'petricca';
+
+var c = a + b;
+console.log(c);
+
+
+var whatever = 10;
+
+if (whatever > 0) {
+    console.log('true');
+} else if (whatever < 10) {
+    console.log('false');
+
+for (var i = 0; i < 10; ++i) {
+    console.log('This loop has run ' + i + ' times');
+}
+
