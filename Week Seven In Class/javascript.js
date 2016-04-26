8a025e3cdd1fe3ef8a6c676561e12d0a39c56fa8
Lindsay Petricca <petrl691@newschool.edu>
2016-04-26 09:09:18 -0400

    commit

--- Week Seven In Class/javascript.js
+++ Week Seven In Class/javascript.js
@@ -1,7 +1,7 @@
-var hello = hello world;
+function printhello () {
 
-console.log(hello);
-
+console.log(hello)
+}
 
 var a = 'lindsay';
 var b = 'petricca';
@@ -20,4 +20,109 @@ if (whatever > 0) {
 for (var i = 0; i < 10; ++i) {
     console.log('This loop has run ' + i + ' times');
 }
+
+
+///////////////////////////////
+
+// Problem 1
+function printHelloWorld () {
+  console.log('hello world')
+}
+
+printHelloWorld();
+
+// Problem 2
+function combineStrings (firstString, secondString) {
+  var resultString = firstString + ' ' + secondString;
+  return resultString;
+}
+
+var combineResult = combineStrings('hello', 'world');
+console.log(combineResult);
+
+// Problem 3
+function saySomething(input) {
+   if (input == 'hello') {
+     return 'hi';
+   } else if (input === 'goodbye') {
+     return 'bye';
+   }
+}
+
+console.log(saySomething('hello'));
+console.log(saySomething('goodbye'));
+
+// Problem 4 
+function isGreaterThanTen (number) {
+  if (number > 10) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isGreaterThanTen(2));
+
+// Problem 5
+function printOneThruTen () {
+  for (var i = 1; i < 11; ++i) {
+    console.log(i);
+  }
+}
+
+printOneThruTen();
+
+
+// Problem 6
+function evaluateOneThruTwenty() {
+  for (var i = 1; i < 21; ++i) {
+    if (i < 10) {
+      console.log('small');
+    } else {
+      console.log('large');
+    }
+  }
+}
+
+evaluateOneThruTwenty();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
